cc47d15c76b1ab9a58243b3122361726a2f0b8c1
Adem Ozenir <ademozenir@gmail.com>
2023-06-21 10:10:12 +0300

    firebase settings edit cloud firestore, firebase authentication and add fingerprint

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ichatty/firebase_options.dart';
 import 'package:ichatty/views/home_view.dart';
 
 late Size mq;
 
 void main() {
+  _initializeFirebase();
     runApp(const MyApp());
 }
 
@@ -26,5 +29,9 @@ class MyApp extends StatelessWidget {
         )),
         home: const HomeView());
   }
+}
+
+_initializeFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
