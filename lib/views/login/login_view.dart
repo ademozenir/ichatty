6df9b9e3f92e77fe70fbb9ce31342d4a1d9975ed
Adem Ozenir <ademozenir@gmail.com>
2023-06-22 11:58:39 +0300

    add login view and creating new widgets

---
+++ lib/views/login/login_view.dart
@@ -0,0 +1,174 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:ichatty/views/home_view.dart';
+import 'package:ichatty/widget/button.dart';
+import 'package:ichatty/widget/square_tile.dart';
+import 'package:ichatty/widget/textfield.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final usernameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        });
+  }
+
+  // handles google login button click
+  _handleGoogleBtnClick() {
+    //for showing progress bar
+
+    _signInWithGoogle().then((user) async {
+      //for hiding progress bar
+      Navigator.pop(context);
+
+      if (user != null) {
+        log('\nUser: ${user.user}');
+        log('\nUserAdditionalInfo: ${user.additionalUserInfo}');
+
+
+          Navigator.pushReplacement(
+              context, MaterialPageRoute(builder: (_) => const HomeView()));
+      }
+    });
+  }
+
+  Future<UserCredential?> _signInWithGoogle() async {
+    try {
+      await InternetAddress.lookup('google.com');
+      // Trigger the authentication flow
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+    } catch (e) {
+      log('\n_signInWithGoogle: $e');
+      return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 15),
+                Image.asset(width: 280, height: 160, "assets/images/logo.png"),
+                const SizedBox(height: 25),
+                Text('Welcome back you\'ve been missed!', style: TextStyle(color: Colors.grey[700], fontSize: 16)),
+                const SizedBox(height: 25),
+                CreateTextField(controller: usernameController, hintText: 'Username', obscureText: false),
+                const SizedBox(height: 15),
+                CreateTextField(controller: passwordController, hintText: 'Password', obscureText: true),
+                const SizedBox(height: 25),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [Text('Forgot Password?', style: TextStyle(color: Colors.grey[600]))],
+                  ),
+                ),
+                const SizedBox(height: 25),
+                CreateButton(
+                  onTap: signUserIn,
+                ),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        child: Text(
+                          'Or continue with',
+                          style: TextStyle(color: Colors.grey[700]),
+                        ),
+                      ),
+                      Expanded(
+                        child: Divider(
+                          thickness: 0.5,
+                          color: Colors.grey[400],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // google button
+                    SquareTile(
+                      imagePath: 'assets/icons/google.png',
+                      onTap: () => _handleGoogleBtnClick(),
+                      // Navigator.push(context, MaterialPageRoute(builder: (_) => HomeView()),
+                    ),
+                    const SizedBox(width: 25),
+                    SquareTile(
+                      imagePath: 'assets/icons/apple.png',
+                      onTap: () {},
+                    )
+                  ],
+                ),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Not a member?',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
+                    const SizedBox(width: 4),
+                    const Text(
+                      'Register now',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widget/chat_user_card.dart
@@ -0,0 +1,51 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:ichatty/main.dart';
+import 'package:ichatty/models/chat_user.dart';
+
+//card to represent a single user in home screen
+class ChatUserCard extends StatefulWidget {
+  final ChatUser user;
+
+  const ChatUserCard({super.key, required this.user});
+
+  @override
+  State<ChatUserCard> createState() => _ChatUserCardState();
+}
+
+class _ChatUserCardState extends State<ChatUserCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.symmetric(horizontal: mq.width * .04, vertical: 4),
+      // color: Colors.blue.shade100,
+      elevation: 0.5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+      child: InkWell(
+        onTap: () {},
+        child: ListTile(
+          //user profile picture
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(mq.height * .03),
+            child: CachedNetworkImage(
+              width: mq.height * .055,
+              height: mq.height * .055,
+              imageUrl: widget.user.image,
+              errorWidget: (context, url, error) => const CircleAvatar(child: Icon(CupertinoIcons.person)),
+            ),
+          ),
+
+          //user name
+          title: Text(widget.user.name),
+
+          //last message
+          subtitle: Text(widget.user.about, maxLines: 1),
+
+          //last message time
+          trailing: const Text("12:00 PM"),
+        ),
+      ),
+    );
+  }
+}
