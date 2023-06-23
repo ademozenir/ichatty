9d5429444a9e99244ff7f3fdda64e75ffdd0db94
Adem Ozenir <ademozenir@gmail.com>
2023-06-23 09:08:16 +0300

    add api service class

---
+++ lib/api/apis.dart
@@ -0,0 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:ichatty/models/chat_user.dart';
+
+class APIs {
+  // for authentication
+  static FirebaseAuth auth = FirebaseAuth.instance;
+
+  // for accessing cloud firestore database
+  static FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  // to return current user
+  static get user => auth.currentUser!;
+
+
+  //  for checking if user exist or not?
+  static Future<bool> userExists() async {
+    return (await firestore.collection("users").doc(user.uid).get()).exists;
+  }
+
+  // for creating a new user
+  static Future<void> createUser() async {
+    final time = DateTime.now().millisecondsSinceEpoch.toString();
+
+    final chatUser = ChatUser(
+        id: user.uid,
+        name: user.displayName.toString(),
+        email: user.email.toString(),
+        about: "Hey, I'm using Chat Night!",
+        image: user.photoURL.toString(),
+        createdAt: time,
+        isOnline: false,
+        lastActive: time,
+        pushToken: '');
+
+    return await firestore.collection('users').doc(user.uid).set(chatUser.toJson());
+  }
+
+}
